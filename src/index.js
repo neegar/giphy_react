4a6613a6c8ac3b1a24e217bc8f2300efa81008b2
Your Name <you@example.com>
2019-05-30 18:40:27 +0400

    colors changed

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ let gifs =[]
 axios.get("https://api.giphy.com/v1/gifs/search?api_key=O5LyiGH753b9EWs8TEN6LL0DLtx8wqNm&q=cat&limit=25&offset=0&rating=G&lang=en").then((response)=> {
     gifs = response.data.data.map((value,index)=>{
 
-        return(<li><img src= {value.images['480w_still'].url}/></li>
+        return(
 
+        <div >
+          <li><img className ="col-4 shadow p-3 mb-5 bg-info rounded" src= {value.images['480w_still'].url}/></li>
+        </div>
         )
 
     })
